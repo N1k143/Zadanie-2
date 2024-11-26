d4bc3246f8209bfbc277de11fd866402c6e83087
nikitapetrunin296.email@example.com <nikitapetrunin296.gmail.com>
2024-11-26 14:11:37 +0500

    first commit

---
+++ src/components/Forma.vue
@@ -0,0 +1,31 @@
+<template>
+    <form>
+      <div class="mb-3">
+        <label for="fio" class="form-label">ФИО сотрудника</label>
+        <input  type="text" class="form-control" id="fio" placeholder="Введите ФИО">
+      </div>
+      <div class="mb-3">
+        <label for="date" class="form-label">Дата выдачи зарплаты</label>
+        <input type="date" class="form-control" id="date">
+      </div>
+      <div class="mb-3">
+        <label for="days" class="form-label">Количество отработанных дней </label>
+        <input type="number" class="form-control" id="days">
+      </div>
+      <div class="mb-3">
+        <label for="" class="form-label">Размер заработной платы сотрудника</label>
+        <input type="number" class="form-control" id="days">
+      </div>
+      <button type="button" class="btn btn-outline-primary">Primary</button>
+    </form>
+    </template>
+    
+    <script>
+    export default {
+        
+    }
+    </script>
+    
+    <style>
+    
+    </style>
