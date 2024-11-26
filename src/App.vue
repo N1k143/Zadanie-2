86e1f2c06675c63668506855171811a17e32fb2b
nikitapetrunin296.email@example.com <nikitapetrunin296.gmail.com>
2024-11-26 14:32:17 +0500

    add form

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
+  <h1 class="text-center">Cтраница учета заработной платы сотрудников</h1>
 <Forma/>
+<h1 class="text-end">Общая сумма: 1500тг</h1>
+
+
 </template>
 
 <script setup>
 import Forma from "./components/Forma.vue";
+
+
 </script>
 
 <style>
