72c7f7db72d143836e97d1da2dd6a3ff8c693adf
nikitapetrunin296.email@example.com <nikitapetrunin296.gmail.com>
2024-11-26 15:00:04 +0500

    add cards

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <h1 class="text-center">Cтраница учета заработной платы сотрудников</h1>
 <Forma/>
+<Cards/>
 <h1 class="text-end">Общая сумма: 1500тг</h1>
-
-
+<h1 class="text-end">Общая сумма с налогом: 1500тг</h1>
 </template>
 
 <script setup>
 import Forma from "./components/Forma.vue";
-
-
+import Cards from "./components/Cards.vue";
 </script>
 
 <style>
